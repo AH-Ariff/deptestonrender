efa6339bcb88ff3fc9eba8c649c208e97ba699be
Arif Hossen <ahariff01@gmail.com>
2025-10-07 21:54:26 +0600

    fix

--- db.js
+++ db.js
@@ -1,11 +1,6 @@
 const sql = require("mysql2/promise");
 require("dotenv").config();
 
-const pool = sql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB,
-});
+const pool = mysql.createPool(process.env.DB_URL);
 
 module.exports = pool;
